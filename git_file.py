eff41ce74f4fe6088cbe2ce1cd4fc99ee6431cae
Slava-ai-one <varyukhinvv2@gmail.com>
2024-11-29 16:59:25 +0400

    This commit from enternet

--- git_file.py
+++ git_file.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('actually a week, im learning a GitHub')
+    print('This print im added with web-changing instruments')
 
 
 if __name__ == '__main__':
